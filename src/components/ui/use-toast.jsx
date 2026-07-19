8a6dd1f6cee3771dc3f1e8b432b3328802a07080
Golden Tech <goldentechcm@gmail.com>
2026-07-19 05:44:54 +0000

    File changes

--- src/components/ui/use-toast.jsx
+++ src/components/ui/use-toast.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 
 const TOAST_LIMIT = 20;
-const TOAST_REMOVE_DELAY = 1000000;
+const TOAST_REMOVE_DELAY = 5000;
 
 const actionTypes = {
   ADD_TOAST: "ADD_TOAST",
@@ -134,6 +134,11 @@ function toast({ ...props }) {
     },
   });
 
+  const duration = props.duration ?? 5000;
+  setTimeout(() => {
+    dismiss();
+  }, duration);
+
   return {
     id,
     dismiss,
@@ -161,4 +166,4 @@ function useToast() {
   };
 }
 
-export { useToast, toast }; 
+export { useToast, toast };
